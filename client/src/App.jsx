1c26740256ec7571e538d245fdd7dfc660b02366
Nawazish Bilal <nawazishbilal@gmail.com>
2026-08-03 02:32:30 +0530

    restarting the frontend from scratch due to compatibility issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 import './App.css'
-import AppRoutes from "./routes/AppRoutes";
 
-/*function App() {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -118,8 +117,6 @@ import AppRoutes from "./routes/AppRoutes";
       <section id="spacer"></section>
     </>
   )
-} */
-
-export default function App() {
-  return <AppRoutes />;
 }
+
+export default App
--- client/src/pages/public/EventDetails.jsx
+++
@@ -1,3 +0,0 @@
-export default function EventDetails() {
-    return <h1>Home</h1>;
-}
